adb3056dd824df0d3cb30c74fd53ebc27318a5d4
c-bertran <clement.bertrand22@gmail.com>
2024-07-14 17:27:05 +0200

    Version 1.0.6-beta

--- gen/slotGen.mjs
+++ gen/slotGen.mjs
@@ -1,31 +1,48 @@
-import { writeFile } from 'fs/promises';
-import { argv } from 'process';
+import { access, constants, mkdir, writeFile } from 'fs/promises';
+import { argv, exit } from 'process';
 import { resolve } from 'path';
 import { minecraft } from '../../api/dist/frontend.js';
 
 (async() => {
-	const blockData = minecraft.get(argv[2], 'block').sort((a, b) => a.name.localeCompare(b.name));
-	const slotPath = {
-		'7b': resolve('.', 'data', 'mapcraft', 'function', 'built_in', 'player', 'get_block', `slot7b.mcfunction`),
-		'8b': resolve('.', 'data', 'mapcraft', 'function', 'built_in', 'player', 'get_block', `slot8b.mcfunction`)
-	};
-	const writeSlot = async (slot) => {
-		console.log('Generate', slot, 'file');
-		await writeFile(slotPath[slot], `# Slot ${slot} - ${argv[2]}\n`, { encoding: 'utf-8', flag: 'w' });
-		for (const block of blockData) {
-			let cleanId = block.name;
-			let data = null;
+	if (argv.length <= 2) {
+		console.log('Pass targeted minecraft version in script parameter');
+		exit(1);
+	}
+	const blockData = minecraft.get(argv[2], 'block')
+		.sort((a, b) => a.name.localeCompare(b.name));
+	const blockSpecificReplace = new Map([
+		// Portal
+		['end_portal', 'nether_star'],
+		['nether_portal', 'ender_pearl'],
+		// Liquid
+		['water', 'water_bucket'],
+		['lava', 'lava_bucket'],
+		// Other
+		['short_grass', 'wheat_seeds'],
+		['powder_snow', 'snowball'],
+		['sweet_berry_bush', 'sweet_berries']
+	]);
 
-			if (cleanId === 'short_grass')
-				continue;
-			if (cleanId === 'water_bucket' || cleanId === 'lava_bucket')
-				cleanId = cleanId.slice(0, cleanId.indexOf('_'));
-			data = (block.name === 'air')
-				? `execute unless entity @s[nbt={Inventory:[{Slot:${slot}}]}]`
-				: `execute if entity @s[nbt={Inventory:[{Slot:${slot},id:"minecraft:${block.name}"}]}]`;
+	const writeSlot = async (dirPath, slot) => {
+		const filePath = resolve(dirPath, `slot${slot}.mcfunction`);
+		console.log('Generate', slot, 'file');
+		try {
+			await access(dirPath, constants.O_RDWR);
+		} catch {
+			await mkdir(dirPath, { recursive: true });
+		}
+		await writeFile(filePath, `# Slot ${slot} - ${argv[2]}\n`, { encoding: 'utf-8', flag: 'w' });
+		for (const block of blockData) {
 			await writeFile(
-				slotPath[slot],
-				`${data} run setblock ~ ~ ~ minecraft:${cleanId}\n`,
+				filePath,
+				`${(block.name === 'air')
+						? `execute unless entity @s[nbt={Inventory:[{Slot:${slot}}]}]`
+						: `execute if entity @s[nbt={Inventory:[{Slot:${slot},id:"minecraft:${
+							(blockSpecificReplace.has(block.name))
+								? blockSpecificReplace.get(block.name)
+								: block.name
+							}"}]}]`
+				} run setblock ~ ~ ~ minecraft:${block.name}\n`,
 				{
 					encoding: 'utf-8',
 					flag: 'a'
@@ -33,6 +50,7 @@ import { minecraft } from '../../api/dist/frontend.js';
 			);
 		}
 	};
-	await writeSlot('7b');
-	await writeSlot('8b');
+
+	await writeSlot(resolve('.', 'data', 'mapcraft', 'function', 'built_in', 'player', 'get_block'), '7b');
+	await writeSlot(resolve('.', 'data', 'mapcraft', 'function', 'built_in', 'player', 'get_block'), '8b');
 })();
